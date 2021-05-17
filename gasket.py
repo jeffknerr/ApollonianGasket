24b2d8cf9587f39fb6612271f6b83d77b15e361c
Jeffrey M. Knerr <knerr@goofydogears.net>
2021-05-17 15:09:10 -0400

    closer...added tangent function, only use tangent circles

--- gasket.py
+++ gasket.py
@@ -16,7 +16,7 @@ from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
 from matplotlib import cm
 import matplotlib.pyplot as plt
-import circles
+import mycircles
 import numpy as np
 
 
@@ -24,7 +24,7 @@ def main():
     """apollonian gasket code"""
     # recursion control...
     level = 0
-    maxdepth = 5
+    maxdepth = 4
     # store all circles, plot at the end
     allcircles = {}
     # first circle C1 that surrounds all the others
@@ -32,21 +32,21 @@ def main():
     y1 = 0
     r1 = 1
     a = -1
-    Cobj = circles.Circle(x1, y1, r1)
+    Cobj = mycircles.Circle(x1, y1, r1)
     allcircles[hash(Cobj)] = Cobj
     # for second circle C2, let user pick x location such that 0>x2<1
     y2 = 0
     x2 = getx("x2", 0, 1)
     r2 = 1 - x2
     b = 1/r2
-    Cobj = circles.Circle(x2, y2, r2)
+    Cobj = mycircles.Circle(x2, y2, r2)
     allcircles[hash(Cobj)] = Cobj
     # now find C3, assuming it also has y3 = 0
     r3 = 1 - r2
     c = 1/r3
     x3 = x2 - r2 - r3
     y3 = 0
-    Cobj = circles.Circle(x3, y3, r3)
+    Cobj = mycircles.Circle(x3, y3, r3)
     allcircles[hash(Cobj)] = Cobj
     # A = bend*center = a*(x1+y1j)
     A = a*(x1+y1*1j)
@@ -58,33 +58,42 @@ def main():
 
 def solve(a, A, b, B, c, C, level, maxdepth, allcircles):
     """given 3 circles, find 2 new ones"""
-    dp, dm = descartes(a, b, c)
-    d = dp   # ??? only use one???
-    Dplus = (A + B + C) + 2*cmath.sqrt(A*B + A*C + B*C)
-    Dminus = (A + B + C) - 2*cmath.sqrt(A*B + A*C + B*C)
-    Dyplus = Dplus.imag/d
-    Dyminus = Dminus.imag/d
-    Dxplus = Dplus.real/d
-    Dxminus = Dminus.real/d
-    # add two new ones to dictionary
-    Cobj = circles.Circle(Dxplus, Dyplus, 1/d)
-    allcircles[hash(Cobj)] = Cobj
-    Cobj = circles.Circle(Dxminus, Dyminus, 1/d)
-    allcircles[hash(Cobj)] = Cobj
     if level > maxdepth:
         return
-    else:
-        level = level + 1
-        # recur on 6 new possibilities
-        solve(a, A, b, B, d, Dplus, level, maxdepth, allcircles)
-        solve(a, A, c, C, d, Dplus, level, maxdepth, allcircles)
-        solve(c, C, b, B, d, Dplus, level, maxdepth, allcircles)
-        solve(a, A, b, B, d, Dminus, level, maxdepth, allcircles)
-        solve(a, A, c, C, d, Dminus, level, maxdepth, allcircles)
-        solve(c, C, b, B, d, Dminus, level, maxdepth, allcircles)
+    dp, dm = descartes(a, b, c)
+    Dplus = (A + B + C) + (2*cmath.sqrt(A*B + A*C + B*C))
+    Dminus = (A + B + C) - (2*cmath.sqrt(A*B + A*C + B*C))
+    # 4 possible solutions, only two of them are tangent to original 3
+    # checkAll(A,a,B,b,C,c,dp,dm,Dplus,Dminus)
+    # find/add two new ones to dictionary by doing tangent checks...
+    for d,D in [(dp,Dplus),(dp,Dminus),(dm,Dplus),(dm,Dminus)]:
+        if not tangent(a,A,b,B,c,C,d,D):
+            pass  # don't include this solution....don't recur on it
+        else:
+            Dx = D.real/d
+            Dy = D.imag/d
+            Cobj = mycircles.Circle(Dx, Dy, 1/d)
+            if hash(Cobj) not in allcircles:
+                allcircles[hash(Cobj)] = Cobj
+            else:
+                level = level + 1
+                # recur on 3 new possibilities
+                solve(a, A, b, B, d, D, level, maxdepth, allcircles)
+                solve(a, A, c, C, d, D, level, maxdepth, allcircles)
+                solve(c, C, b, B, d, D, level, maxdepth, allcircles)
 
-#   print("plus: dx = %.2f  dy = %.2f" % (Dxplus, Dyplus))
-#   print("minus: dx = %.2f  dy = %.2f" % (Dxminus, Dyminus))
+
+def printc(text,X,x,Y,y,Z,z):
+    """print circle details"""
+    xi = (X/x).imag
+    xr = (X/x).real
+    yi = (Y/y).imag
+    yr = (Y/y).real
+    zi = (Z/z).imag
+    zr = (Z/z).real
+    print("%s: %5.2f,%5.2f %5.2f |%5.2f,%5.2f %5.2f |%5.2f,%5.2f %5.2f" %
+            (text,xr,xi,1/x,yr,yi,1/y,zr,zi,1/z))
+
 
 
 def plot(allcircles):
@@ -144,7 +153,7 @@ def descartes(a, b, c):
        d = a + b + c +/- 2*sqrt(ab + ac + bc)
     """
     radicand = a*b + a*c + b*c
-    epsilon = 1.0e-8
+    epsilon = 1.0e-4
     if abs(radicand) < epsilon:
         radicand = 0
     try:
@@ -152,8 +161,40 @@ def descartes(a, b, c):
         minus = a + b + c - 2*math.sqrt(radicand)
         return plus, minus
     except ValueError:
-        print("got value error trying sqrt of %d..." % (radicand))
+        print("got value error trying sqrt of %f..." % (radicand))
         return None
+
+
+def tangent(a,A,b,B,c,C,d,D):
+    """return True if circles are all tangent to each other"""
+    return twoTangent(a,A,d,D) and \
+           twoTangent(b,B,d,D) and \
+           twoTangent(c,C,d,D)
+
+
+def twoTangent(a,A,b,B):
+    """return True if two circles are tangent"""
+    r1 = abs(1/a)
+    r2 = abs(1/b)
+    x1 = A.real/a
+    y1 = A.imag/a
+    x2 = B.real/b
+    y2 = B.imag/b
+    d = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    # allow for approx equal
+    epsilon = 1.0e-4
+    diff1 = abs(d-(r1+r2))  # test for tangent OUTSIDE circle
+    diff2 = abs(d-abs(r1-r2))  # test for tangent INSIDE circle
+    return (diff1 <= epsilon) or (diff2 <= epsilon)
+
+
+def checkAll(A,a,B,b,C,c,dp,dm,Dplus,Dminus):
+    """check which ones are tangent"""
+    print("Dpdp: ", tangent(a,A,b,B,c,C,dp,Dplus))
+    print("Dpdm: ", tangent(a,A,b,B,c,C,dp,Dminus))
+    print("Dmdp: ", tangent(a,A,b,B,c,C,dm,Dplus))
+    print("Dmdm: ", tangent(a,A,b,B,c,C,dm,Dminus))
+    print("-"*20)
 
 
 main()
