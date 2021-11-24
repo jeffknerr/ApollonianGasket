86b478cb9927019586202b0c73e9089278fec841
Jeffrey M. Knerr <knerr@cs.swarthmore.edu>
2021-11-24 10:27:39 -0500

    added colors, added to readme, removed debugging

--- README.md
+++ README.md
@@ -1,8 +1,54 @@
 # ApollonianGasket
 Apollonian Gasket
 
-https://www.americanscientist.org/article/a-tisket-a-tasket-an-apollonian-gasket
+- [Dana Mackenzie AmSci articel](https://www.americanscientist.org/article/a-tisket-a-tasket-an-apollonian-gasket)
+- [useful Ivars Peterson pdf](https://maths.ac-noumea.nc/amc/docs/circle_game.pdf)
+- [Lagarias, Mallows, Wilks paper](https://arxiv.org/abs/math/0101066)
 
-https://maths.ac-noumea.nc/amc/docs/circle_game.pdf
+![apollonian gasket picture](apolloniangasket.png)
 
-![apolloniangasket.png](apolloniangasket.png)
+![grey gasket picture](greys.png)
+
+![rainbow gasket picture](rainbow.png)
+
+- assumes circle #1 has radius=1 and contains all other 
+circles (i.e., has a *negative* curvature or bend)
+- assumes circles #2 and #3 both have y=0
+- allows user to choose x of circle #2 (which determines
+center and radius of circle #3)
+- learned some matplotlib ([patches](https://matplotlib.org/stable/api/_as_gen/matplotlib.patches.Circle.html))
+- learned python has complex numbers!
+- learned the [click library](https://click.palletsprojects.com/en/8.0.x/)
+
+## algorithm summary
+
+- given 3 circles, all tangent, where first circle contains other two
+- calculate bends for each circle:
+```
+a = -1
+b = 1/r2
+c = 1/r3
+```
+- calculate bend-times-center for each circle:
+```
+A = a*(x1 + y1*j)
+B = b*(x2 + y2*j)
+C = c*(x3 + y3*j)
+```
+- use Descartes' formula to calculate `d=1/r4` (with two possible solutions)
+```
+d = a + b + c +/- 2*sqrt(ab + ac + bc)
+```
+- use Lagarias/Mallows/Wilks formula to calculate `D=d(x4 + y4*j)` (also 
+with two possible solutions)
+```
+D = (A + B + C) +/- (2*cmath.sqrt(A*B + A*C + B*C))
+```
+- we now have a fourth circle...recur on possible combinations:
+```
+given A, B, D, find next tangent circle to those three
+given A, C, D, find next tangent circle to those three
+given C, B, D, find next tangent circle to those three
+```
+- keep going until some max level of recursion is reached (e.g., 7)
+- save all circles as you go, plot them at the end
--- gasket.py
+++ gasket.py
@@ -24,23 +24,25 @@ import click
               help="x coordinate of circle #2 (e.g., 0.3 or 0.5)")
 @click.option("--maxdepth", default=4,
               help="max recursion depth (e.g., 3, 7, 2)")
-def main(x2, maxdepth):
+@click.option("--palette", default="blues",
+              help="rainbow|blues|greys palette")
+def main(x2, maxdepth, palette):
     """apollonian gasket code"""
     # recursion control...
     level = 0
-    # maxdepth = 5
     # store all circles, plot at the end
     allcircles = {}
     # first circle C1 that surrounds all the others
     x1 = 0
     y1 = 0
     r1 = 1
-    a = -1
+    a = -1   # first circle is large and contains all others
     Cobj = mycircles.Circle(x1, y1, r1)
     allcircles[hash(Cobj)] = Cobj
-    # for second circle C2, let user pick x location such that 0>x2<1
+    # for second circle C2, user picks x location such that 0>x2<1
     y2 = 0
-    # x2 = getx("x2", 0, 1)
+    if x2 <= 0 or x2 >= 1:
+        x2 = getx("x2", 0, 1)
     r2 = 1 - x2
     b = 1/r2
     Cobj = mycircles.Circle(x2, y2, r2)
@@ -57,7 +59,7 @@ def main(x2, maxdepth):
     B = b*(x2+y2*1j)
     C = c*(x3+y3*1j)
     solve(a, A, b, B, c, C, level, maxdepth, allcircles)
-    plot(allcircles)
+    plot(allcircles, palette)
 
 
 def solve(a, A, b, B, c, C, level, maxdepth, allcircles):
@@ -68,17 +70,14 @@ def solve(a, A, b, B, c, C, level, maxdepth, allcircles):
     Dplus = (A + B + C) + (2*cmath.sqrt(A*B + A*C + B*C))
     Dminus = (A + B + C) - (2*cmath.sqrt(A*B + A*C + B*C))
     # 4 possible solutions, only two of them are tangent to original 3
-    # checkAll(A,a,B,b,C,c,dp,dm,Dplus,Dminus)
     # find/add two new ones to dictionary by doing tangent checks...
-#   print("level:", level)
-#   printc("ABC:",A,a,B,b,C,c)
-#   printc("pp,mp,pm,mm:",Dplus,dp,Dminus,dp,Dplus,dm,Dminus,dm)
-#   input()
     for d, D in [(dp, Dplus),  (dp, Dminus), (dm, Dplus), (dm, Dminus)]:
         if not tangent(a, A, b, B, c, C, d, D):
             pass  # don't include this solution....don't recur on it
+            # not sure why there *are* "solutions" that aren't tangent..
         elif d < 0:
-            pass  # don't allow negative radii ???
+            pass  # don't allow negative radii 
+            # these are just duplicates of the original first circle
         else:
             Dx = D.real/d
             Dy = D.imag/d
@@ -90,24 +89,6 @@ def solve(a, A, b, B, c, C, level, maxdepth, allcircles):
                 solve(a, A, b, B, d, D, level, maxdepth, allcircles)
                 solve(a, A, c, C, d, D, level, maxdepth, allcircles)
                 solve(c, C, b, B, d, D, level, maxdepth, allcircles)
-
-
-def printc(text, X, x, Y, y, Z, z, W=(99+99j), w=99):
-    """print circle details"""
-    xi = (X/x).imag
-    xr = (X/x).real
-    yi = (Y/y).imag
-    yr = (Y/y).real
-    zi = (Z/z).imag
-    zr = (Z/z).real
-    wi = (W/w).imag
-    wr = (W/w).real
-    if w == 99:
-        print("%s: %5.2f, %5.2f %5.2f |%5.2f, %5.2f %5.2f |%5.2f, %5.2f %5.2f" %
-              (text, xr, xi, 1/x, yr, yi, 1/y, zr, zi, 1/z))
-    else:
-        print("%s: %5.2f, %5.2f %5.2f |%5.2f, %5.2f %5.2f |%5.2f, %5.2f %5.2f |%5.2f, %5.2f %5.2f" %
-              (text, xr, xi, 1/x, yr, yi, 1/y, zr, zi, 1/z, wr, wi, 1/w))
 
 
 def getx(prompt, minx, maxx):
@@ -170,40 +151,12 @@ def twoTangent(a, A, b, B):
     d = math.sqrt((x2-x1)**2 + (y2-y1)**2)
     # allow for approx equal
     epsilon = 1.0e-4
-    diff1 = abs(d-(r1+r2))  # test for tangent OUTSIDE circle
+    diff1 = abs(d-(r1+r2))     # test for tangent OUTSIDE circle
     diff2 = abs(d-abs(r1-r2))  # test for tangent INSIDE circle
     return (diff1 <= epsilon) or (diff2 <= epsilon)
 
 
-def checkAll(A, a, B, b, C, c, dp, dm, Dplus, Dminus):
-    """check which ones are tangent"""
-    print("Dpdp: ", tangent(a, A, b, B, c, C, dp, Dplus))
-    print("Dpdm: ", tangent(a, A, b, B, c, C, dp, Dminus))
-    print("Dmdp: ", tangent(a, A, b, B, c, C, dm, Dplus))
-    print("Dmdm: ", tangent(a, A, b, B, c, C, dm, Dminus))
-    print("-"*20)
-
-
-def mysort(allcircles):
-    """return keys, sorted by circle radius (largest first)"""
-    # note: need to do my own sort, since the Circle objects already
-    # have an __eq__ method that is not related to radius...
-    keys = list(allcircles.keys())
-    values = list(allcircles.values())
-    for i in range(1, len(keys)):
-        key = keys[i]                # save current key
-        value = values[i]            # save current value
-        posn = i
-        while posn > 0 and value.getR() > values[posn-1].getR():
-            values[posn] = values[posn-1]  # move item right one position
-            keys[posn] = keys[posn-1]
-            posn = posn - 1
-        values[posn] = value     # put it back at the end, in correct spot
-        keys[posn] = key         # put it back at the end, in correct spot
-    return keys
-
-
-def plot(allcircles):
+def plot(allcircles, palette):
     """plot the circles"""
     fig, ax = plt.subplots()
     patches = []
@@ -213,28 +166,31 @@ def plot(allcircles):
     ax.set_aspect(1)
     ax.set_title("Apollonian Gasket")
 
-    # sort keys based on circle radius, so we can plot largest first
-    keys = mysort(allcircles)
-#   keys.reverse()
-    for key in keys[0:len(keys) - 1]:    # remove duplicates???
+    for key in allcircles:
         cobj = allcircles[key]
-        print(cobj)
-# todo: why are there similar circles in the list??? (0,0,-1) <-- 3 of these??
         x = cobj.getX()
         y = cobj.getY()
         radius = cobj.getR()
         circle = Circle((x, y), radius)
         patches.append(circle)
-    mycolors = ["red", "green", "blue", "cyan", "pink", "orange"]
-    blues = [ "LightSlateGrey", "grey", "LightGrey",
-"LightGray", "MidnightBlue", "navy",
-"CornflowerBlue", "DarkSlateBlue", "SlateBlue", "MediumSlateBlue",
-"MediumBlue", "RoyalBlue", "blue",
-"DodgerBlue", "DeepSkyBlue", "SkyBlue", "LightSkyBlue",
-"SteelBlue", "LightSteelBlue", "LightBlue", "PowderBlue",
-            ]
+    rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", 
+               "violet"]
+    blues = [ "LightSlateGrey", "grey", "LightGrey", 
+             "LightGray", "MidnightBlue", "navy",
+             "CornflowerBlue", "DarkSlateBlue", "SlateBlue", 
+             "MediumSlateBlue", "MediumBlue", "RoyalBlue", "blue",
+             "DodgerBlue", "DeepSkyBlue", "SkyBlue", "LightSkyBlue",
+             "SteelBlue", "LightSteelBlue", "LightBlue", "PowderBlue"]
+    greys = [ "white", "black", "LightSlateGrey", "grey", "LightGrey",
+            "DarkGrey", "DimGrey", "SlateGrey"]
+    if palette == "rainbow":
+        palette = rainbow
+    elif palette == "greys":
+        palette = greys
+    else:
+        palette = blues
     collection = PatchCollection(patches, edgecolor="black",
-                                 facecolor=blues,
+                                 facecolor=palette,
                                  alpha=1.0)
     ax.add_collection(collection)
     plt.show()
